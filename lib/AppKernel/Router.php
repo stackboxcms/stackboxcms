d56655b8745c18418e96f25f775290a0f60e1949
Vance Lucas <vance@vancelucas.com>
2009-11-25 16:23:02 -0600

    AppKernel: Initial version, derived from Spot

---
+++ lib/AppKernel/Router.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * Router
+ *
+ * Maps URL to named parameters for use in application
+ *
+ * @package AppKernel
+ * @author Vance Lucas <vance@vancelucas.com>
+ */
+class AppKernel_Router
+{
+	// Store specified routes
+	protected $_routes = array();
+	protected $_defaultRoute = array();
+	
+	// Default regex match syntax to replace named keys with
+	protected $_paramRegex = "([a-zA-Z0-9\_\-]+)";
+	protected $_paramRegexNumeric = "([0-9]+)";
+	protected $_paramRegexWildcard = "(.*)";
+	
+	
+	/**
+	 * Connect route
+	 */
+	public function route($route, array $defaults = array(), $name = null)
+	{
+		if($name === null) {
+			// Use numeric index if name is not given
+			$name = count($this->routes());
+		}
+		
+		$routeRegex = null;
+		$routeParams = array();
+		preg_match_all("@\([\:|\*|\#]([^\)]+)\)@", $route, $regexMatches, PREG_PATTERN_ORDER);
+		if(isset($regexMatches[1])) {
+			$routeParams = $regexMatches[1];
+			// Replace all keys with regex
+			$routeRegex = $route;
+			foreach($routeParams as $paramName) {
+				// Does parameter have marker for custom regex rule?
+				if(strpos($paramName, '|') !== false) {
+					// Custom regex rule
+					$paramParts = explode('|', $paramName);
+					$routeRegex = str_replace("(:" . $paramName . ")", "(" . $paramParts[1] . ")", $routeRegex);
+				} else {
+					// Standard regex rule
+					$routeRegex = str_replace("(:" . $paramName . ")", $this->_paramRegex, $routeRegex);
+					$routeRegex = str_replace("(#" . $paramName . ")", $this->_paramRegexNumeric, $routeRegex);
+					$routeRegex = str_replace("(*" . $paramName . ")", $this->_paramRegexWildcard, $routeRegex);
+				}
+			}
+		}
+		
+		// Save route info
+		$routeArray = array(
+			'route' => $route,
+			'regex' => $routeRegex,
+			'params' => $routeParams,
+			'defaults' => $defaults
+		);
+	
+		// The first route defined is the default one
+		if(count($this->routes()) == 0) {
+			$this->_defaultRoute = $routeArray;
+		}
+		
+		// Save route info
+		$this->_routes[$name] = $routeArray;
+	}
+	
+	
+	/**
+	 * Get set routes
+	 */
+	public function routes()
+	{
+		return $this->_routes;
+	}
+	
+	
+	/**
+	 * Match given URL string
+	 *
+	 * @param string $url
+	 * @return array $params Parameters with values that were matched
+	 */
+	public function match($url)
+	{
+		if(count($this->routes()) == 0) {
+			throw new OutOfBoundsException("There must be at least one route defined to match for.");
+		}
+		
+		// Clean up URL for matching
+		$url = trim(urldecode($url), '/');
+		$params = array();
+		
+		// Loop over set routes to find a match
+		// Order matters - Looping will stop when first suitable match is found
+		$routes = $this->routes();
+		foreach($routes as $routeName => $route) {
+			if($params = $this->routeMatch($route, $url)) {
+				break;
+			}
+        }
+		
+		// Use default route params if no match
+		if(count($params) == 0) {
+			$params = $this->_defaultRoute['defaults'];
+		}
+		
+		return $params;
+	}
+	
+	
+	/**
+	 * Match URL against a specific given route
+	 */
+	protected function routeMatch($route, $url)
+	{
+		$params = array();
+		// Strings are padded at beginning and end, resulting in a more exact (strict) match
+		$result = preg_match("@##" . $route['regex'] . "##@", "##" . $url . "##", $matches);
+		if($result) {
+			array_shift($matches); // Shift off first "match" result - full URL input string
+			$params = array_combine($route['params'], $matches);
+			$params = array_merge($route['defaults'], $params);
+		}
+		return $params;
+	}
+	
+	
+	/**
+	 * Clear existing routes to start over
+	 */
+	public function reset()
+	{
+		$this->_routes = array();
+		$this->_defaultRoute = array();
+	}
+}
