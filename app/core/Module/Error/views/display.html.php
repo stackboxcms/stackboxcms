f7f0ffec3a85e3113ad9652ffbd9c6cfa80aac26
Vance Lucas <vance@vancelucas.com>
2009-12-28 23:05:32 -0600

    Error Module: Added template for error display

---
+++ app/core/Module/Error/views/display.html.php
@@ -0,0 +1,6 @@
+<h1><?php echo $title; ?></h1>
+<?php if($this->errorMessage): ?>
+	<p><?php echo $this->errorMessage; ?></p>
+<?php else: ?>
+	<p>Oops! An error occured that has prevented this page from displaying properly.</p>
+<?php endif; ?>
