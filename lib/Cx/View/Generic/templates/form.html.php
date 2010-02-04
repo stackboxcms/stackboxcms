89731b014b3ac48f767ad287e975e5a4e11812e9
Vance Lucas <vance@vancelucas.com>
2010-02-04 16:26:04 -0600

    Lib: Generic form template update with new CSS naming convention

--- lib/Cx/View/Generic/templates/form.html.php
+++ lib/Cx/View/Generic/templates/form.html.php
@@ -4,20 +4,20 @@ $form = $this->helper('Form');
 
 <?php if($this->fields && count($fields) >0): ?>
 <form action="" method="post">
-  <dl class="cx-form-layout">
+  <dl class="cx_form">
   <?php foreach($fields as $fieldName => $fieldOpts): ?>
-	<dt class="cx-form-label"><label><?php echo $fieldName; ?></label></dt>
-	<dd class="cx-form-value cx-form-field-<?php echo strtolower($fieldOpts['type']); ?>">
+	<dt class="cx_form_label"><label><?php echo $fieldName; ?></label></dt>
+	<dd class="cx_form_value cx_form_field_<?php echo strtolower($fieldOpts['type']); ?>">
 	  <?php echo $form->input($fieldOpts['type'], $fieldName, $request->$fieldName); ?>
 	</dd>
   <?php endforeach; ?>
   </dl>
-  <div class="cx-form-hidden">
+  <div class="cx_form_hidden">
 	<input type="hidden" name="_method" value="<?php echo $this->method; ?>" />
   </div>
-  <div class="cx-form-actions">
-	<button type="submit" class="cx-action-primary">Save</button>
-	<a href="#" class="cx-action-cancel">Cancel</a>
+  <div class="cx_form_actions">
+	<button type="submit" class="cx_action_primary">Save</button>
+	<a href="#" class="cx_action_cancel">Cancel</a>
   </div>
 </form>
 <?php endif; ?>
