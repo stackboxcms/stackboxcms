6b15cf33be24d1b66db90696f9b7a589c43bb3a5
Vance Lucas <vance@vancelucas.com>
2010-10-18 22:36:04 -0500

    Fixed typo in datagrid template

--- lib/Alloy/View/Generic/templates/datagrid.html.php
+++ lib/Alloy/View/Generic/templates/datagrid.html.php
@@ -5,7 +5,7 @@ $i = 0;
 <table cellpadding=="0" cellspacing="0" border="0" class="app_datagrid">
   <?php foreach($this->data as $data => $item): $i++; ?>
     <tr class="app_datagrid_row <?php echo ($i % 2) ? 'even' : 'odd'; ?>">
-    <?php foreach($this->column as $colName => $colOpts):
+    <?php foreach($this->columns as $colName => $colOpts):
         $colLabel = isset($fieldOpts['title']) ? $fieldOpts['title'] : ucwords(str_replace('_', ' ', $colName));
     ?>
       <td class="app_datagrid_cell"><?php echo $colOpts['callback']($item, $this); ?></td>
