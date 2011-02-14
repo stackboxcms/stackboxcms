5292c424892ce0eb319f07b802acfded63205ee9
Vance Lucas <vance@vancelucas.com>
2011-02-14 15:43:30 -0600

    Blog list view: Fixed for updated Alloy libs

--- www/content/Module/Blog/views/editAction.html.php
+++ www/content/Module/Blog/views/editAction.html.php
@@ -1,12 +1,12 @@
 
 <div class="module_blog">
-    <p><a href="<?php echo $this->kernel->url(array('page' => $this->page->url, 'module_name' => 'blog_post', 'module_id' => $this->module->id, 'module_action' => 'new'), 'module'); ?>">New Post</a></p>
+    <p><a href="<?php echo $kernel->url(array('page' => $this->page->url, 'module_name' => 'blog_post', 'module_id' => $module->id, 'module_action' => 'new'), 'module'); ?>">New Post</a></p>
 <?php
-$table = new \Alloy\View\Generic\Datagrid('datagrid');
-$table->data($this->posts)
-    ->column('Post', function($item, $view) { return $item->title; })
-    ->column('Status', function($item, $view) { return $item->status; })
-    ->column('Date Published', function($item, $view) { return $item->date_published; });
+$table = $view->generic('datagrid');
+$table->data($posts)
+    ->column('Post', function($item) { return $item->title; })
+    ->column('Status', function($item) { return $item->status; })
+    ->column('Date Published', function($item) { return $item->date_published; });
 echo $table;
 ?>
 </div>
