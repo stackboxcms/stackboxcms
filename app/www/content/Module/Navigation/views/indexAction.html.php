46bcee56f1890ffc18f6aab83d8ed61c04c5a94e
Vance Lucas <vance@vancelucas.com>
2011-05-16 14:00:32 -0500

    Module > Navigation: Using new TreeView display and finally ditching hack-ey garbage

--- app/www/content/Module/Navigation/views/indexAction.html.php
+++ app/www/content/Module/Navigation/views/indexAction.html.php
@@ -1,34 +1,12 @@
 <?php
-/**
- * Wrap everything in a function for now so we can display subpages recursively
- * Kind of a hack... There are *much* better ways to do this but I'm under deadline...
- * @todo Fix this garbage and replace with view helpers and proper decorators, etc.
- */
-if(!function_exists('renderNavigation')) { // Can't even believe I am doing this...
-function renderNavigation($pages) {
-    global $kernel;
-    if(!$pages || count($pages) == 0) {
-        echo 'NO PAGES';
-        return;
-    }
-?>
-<ul>
-<?php foreach($pages as $page): ?>
-    <li>
-    <a href="<?php echo $kernel->url(array('page' => ltrim($page->url, '/')), 'page'); ?>" title="<?php echo $page->title; ?>"><?php echo $page->title; ?></a>
-    <?php
-        $children = $page->children;
-        if($children && count($children) > 0):
-            echo renderNavigation($children);
-        endif;
-    ?>
-    </li>
-<?php endforeach; ?>
-</ul>
-<?php
-}
-}
-
-// Render navigation
-renderNavigation($pages);
+// Use generic TreeView to recursively display links
+$tree = $view->generic('treeview')
+    ->data($pages)
+    ->item(function($page) use($view) {
+        return '<a href="' . $view->url(array('page' => ltrim($page->url, '/')), 'page') . '" title="' . $page->title . '">' . $page->title . '</a>';
+    })
+    ->itemChildren(function($page) {
+        return $page->children;
+    });
+echo $tree->content();
 ?>
