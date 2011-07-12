ab30e7f4ce1e75ac900130051d4ad7a44ed1f29f
Vance Lucas <vance@vancelucas.com>
2011-07-11 22:28:52 -0500

    Module > Navigation: Updated view to use settings and 'filter' callback

--- app/www/content/Module/Navigation/views/indexAction.html.php
+++ app/www/content/Module/Navigation/views/indexAction.html.php
@@ -1,7 +1,7 @@
 <?php
 // Use generic TreeView to recursively display links
-$tree = $view->generic('treeview')
-    ->data($pages)
+$tree = $view->generic('treeview');
+$tree->data($pages)
     ->item(function($page) use($view) {
         return '<a href="' . $view->url(array('page' => ltrim($page->url, '/')), 'page') . '" title="' . $page->title . '">' . $page->title . '</a>';
     })
@@ -11,9 +11,20 @@ $tree = $view->generic('treeview')
     ->levelMin($module->setting('level_min', 0)-1)
     ->levelMax($module->setting('level_max', 99)-1)
     ->filter(function($page) use($module) {
-        // Setting: Show Homepage
-        if($page->url == '/' && !$module->setting('show_homepage', true)) {
+        // Setting: Show Homepage?
+        if('/' == $page->url && !$module->setting('show_homepage', true)) {
             return false;
         }
+    })
+    // Custom root CSS class via module setting
+    ->beforeItemSet(function() use($module, $tree) {
+        if(0 == $tree->level()) {
+            return "<ul class=\"" . $module->setting('css_ul_root', 'cms_nav') . "\">\n";   
+        } else {
+            return "<ul>\n";
+        }
+    })
+    ->beforeItem(function() use($module) {
+        return "<li>\n";
     });
 echo $tree->content();
