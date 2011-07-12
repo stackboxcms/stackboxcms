0768131cd5fefca0d287551e10ccdaef4a084a27
Vance Lucas <vance@vancelucas.com>
2011-07-11 22:27:07 -0500

    Alloy View lib update: Added 'Data' helper and updated Treeviwe generic with 'level' method that returns the current tree level

--- alloy/lib/Alloy/View/Generic/Treeview.php
+++ alloy/lib/Alloy/View/Generic/Treeview.php
@@ -220,6 +220,17 @@ class Treeview extends Template
         $this->set('levelMax', $level);
         return $this;
     }
+
+
+    /**
+     * Get currnet level
+     *
+     * @param int $level Current level
+     */
+    public function level()
+    {
+        return self::$_level;
+    }
     
     
     /**
