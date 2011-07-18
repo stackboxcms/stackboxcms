311ef1c86813338dc810c19ea91f98130ac14a4e
Vance Lucas <vance@vancelucas.com>
2011-07-18 12:10:19 -0500

    Slideshow: Updated to use jCycle Lite

--- app/www/content/Module/Slideshow/views/indexAction.html.php
+++ app/www/content/Module/Slideshow/views/indexAction.html.php
@@ -1,45 +1,28 @@
 <?php
-$slideshowId = 'slideshow_' . $module->id;
+$slideshowId = 'module_slideshow_' . $module->id;
 $assetsUrl = $kernel->config('url.root') . $kernel->config('cms.dir.modules') . 'Module/' . $module->name . '/assets/';
 ?>
 
-<div id="<?php echo $slideshowId; ?>" class="slideshow_wrapper">
-  <div class="slideshow_container">
-<?php foreach($items as $item): ?>
-    <div class="slideshow_item">
+<div id="<?php echo $slideshowId; ?>" class="module_slideshow_wrapper">
+    <?php foreach($items as $item): ?>
+    <div class="module_slideshow_item">
         <?php if($item->link): ?><a href="<?php echo $item->link; ?>"><?php endif; ?><img src="<?php echo $item->url; ?>" alt="<?php echo $item->caption; ?>" /><?php if($item->link): ?></a><?php endif; ?>
         <?php if($item->caption): ?>
-        <div class="slideshow_item_caption"><p><?php echo $item->caption; ?></p></div>
+        <div class="module_slideshow_item_caption"><p><?php echo $item->caption; ?></p></div>
         <?php endif; ?>
     </div>
-<?php endforeach; ?>
-  </div>
+    <?php endforeach; ?>
 </div>
 
 <script type="text/javascript">
-$('#<?php echo $slideshowId; ?>').slides({
-    container: 'slideshow_container',
-    preload: true,
-    preloadImage: '<?php echo $assetsUrl; ?>images/loading.gif',
-    play: <?php echo (int) $module->setting('slide_delay', 5000); ?>,
-    pause: <?php echo (int) $module->setting('slide_delay', 5000); ?>,
-    slideSpeed: <?php echo (int) $module->setting('slide_speed', 5000); ?>,
-    hoverPause: true,
-    crossfade: true,
-    animationStart: function(current) {
-        $('.slideshow_item_caption').stop().animate({
-            bottom: -35
-        },100);
-    },
-    animationComplete: function(current) {
-        $('.slideshow_item_caption').stop().animate({
-            bottom:0
-        },200);
-    },
-    slidesLoaded: function() {
-        $('.slideshow_item_caption').stop().animate({
-            bottom:0
-        },200);
+// @link http://jquery.malsup.com/cycle/lite/
+$('#<?php echo $slideshowId; ?>').cycle({ 
+    delay:  <?php echo (int) $module->setting('slide_delay', 5000); ?>, 
+    speed:  <?php echo (int) $module->setting('slide_speed', 2000); ?>,
+    pause:  true,
+    height: <?php echo (int) $module->setting('height', 300); ?>,
+    before: function() {
+        console.log(this);
     }
 });
 </script>
@@ -47,10 +30,5 @@ $('#<?php echo $slideshowId; ?>').slides({
 <?php
 // Add javsacript for slideshow to page
 $asset = $view->helper('Asset');
-$view->head()->script($assetsUrl . '/scripts/jquery.slides.min.js');
-$view->head()->append('
-<style type="text/css">
-    #' . $slideshowId . ' .slides_container { position: relative; width: ' . (int) $module->setting('width', 400) . 'px; display: none; }
-    #' . $slideshowId . ' .slideshow_item { width: ' . (int) $module->setting('width', 400) . 'px; height: ' . (int) $module->setting('height', 300) . 'px; display: block; }
-</style>');
+$view->head()->script($assetsUrl . '/scripts/jquery.cycle.lite.min.js');
 ?>
