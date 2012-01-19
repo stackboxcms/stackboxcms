58d0e803c82905321560a7d30bc6b697948b3725
Vance Lucas <vance@vancelucas.com>
2012-01-19 11:12:01 -0600

    Defined local $kernel var in 'regionModuleFormat'

--- app/Module/Page/Controller.php
+++ app/Module/Page/Controller.php
@@ -498,6 +498,7 @@ class Controller extends Stackbox\Module\ControllerAbstract
      */
     protected function regionModuleFormat($request, $page, $module, $user, $moduleResponse, $includeControls = true)
     {
+        $kernel = \Kernel();
         $content = "";
         if(false === $moduleResponse) {
             $content = false;
@@ -513,7 +514,7 @@ class Controller extends Stackbox\Module\ControllerAbstract
                 // Alloy Module Response type
                 if($moduleResponse instanceof \Alloy\Module\Response) {
                     // Pass HTTP status code
-                    $response = $this->kernel->response($moduleResponse->status());
+                    $response = $kernel->response($moduleResponse->status());
 
                     // Display errors
                     if($errors = $moduleResponse->errors()):
@@ -546,7 +547,7 @@ class Controller extends Stackbox\Module\ControllerAbstract
                   <div class="cms_ui cms_ui_controls">
                     <div class="cms_ui_title"><span>' . $module->name . '</span></div>
                     <ul>
-                      <li><a href="' . $this->kernel->url(array('page' => $page->url, 'module_name' => ($module->name) ? $module->name : $this->name(), 'module_id' => (int) $module->id, 'module_action' => 'editlist'), 'module') . '">Edit</a></li>';
+                      <li><a href="' . $kernel->url(array('page' => $page->url, 'module_name' => ($module->name) ? $module->name : $this->name(), 'module_id' => (int) $module->id, 'module_action' => 'editlist'), 'module') . '">Edit</a></li>';
                       
                       // Options submenu
                       $content .= '
@@ -557,9 +558,9 @@ class Controller extends Stackbox\Module\ControllerAbstract
                       if($module->id > 0) {
                         // Settings link
                         $content .= '
-                        <li><a href="' . $this->kernel->url(array('page' => $page->url, 'module_name' => ($module->name) ? $module->name : $this->name(), 'module_id' => (int) $module->id, 'module_action' => 'settings'), 'module') . '">Settings</a></li>';
+                        <li><a href="' . $kernel->url(array('page' => $page->url, 'module_name' => ($module->name) ? $module->name : $this->name(), 'module_id' => (int) $module->id, 'module_action' => 'settings'), 'module') . '">Settings</a></li>';
                         $content .= '
-                        <li><a href="' . $this->kernel->url(array('page' => $page->url, 'module_name' => 'page_module', 'module_id' => 0, 'module_item' => (int) $module->id, 'module_action' => 'delete'), 'module_item') . '">Delete</a></li>';
+                        <li><a href="' . $kernel->url(array('page' => $page->url, 'module_name' => 'page_module', 'module_id' => 0, 'module_item' => (int) $module->id, 'module_action' => 'delete'), 'module_item') . '">Delete</a></li>';
                       }
                       $content .= '
                        </ul>
